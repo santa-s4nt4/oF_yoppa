47c5ea483f571b76f2b8366b927b63ea9e1a3422
santa-sukitoku <santanaruse@gmail.com>
2020-02-05 18:18:16 +0900

    add circle

--- yoppa_1/src/ofApp.cpp
+++ yoppa_1/src/ofApp.cpp
@@ -2,17 +2,27 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	ofSetFrameRate(60);
+	ofSetBackgroundColor(0);
+	location = ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2);
+	velocity = ofVec2f(ofRandom(-10, 10), ofRandom(-10, 10));
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	location += velocity;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	ofSetColor(31, 12, 255);
+	ofDrawCircle(location, 40);
+	if (location.x < 0 || location.x > ofGetWidth()) {
+		velocity.x *= -1;
+	}
+	if (location.y < 0 || location.y > ofGetWindowHeight()) {
+		velocity.y *= -1;
+	}
 }
 
 //--------------------------------------------------------------
