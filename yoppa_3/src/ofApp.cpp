b95892f6349641527e662504909b08e0a62882b1
santa-sukitoku <santanaruse@gmail.com>
2020-02-10 01:42:16 +0900

    3Dプリミティブ図形の描画

--- yoppa_3/src/ofApp.cpp
+++ yoppa_3/src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	ofBackground(0);
 }
 
 //--------------------------------------------------------------
@@ -12,60 +12,14 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+	ofSetColor(255);
 
-}
+	box.set(200);
+	box.setPosition(-150, 0, 0);
+	box.drawWireframe();
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+	sphere.set(100, 16);
+	sphere.setPosition(150, 0, 0);
+	sphere.drawWireframe();
 }
